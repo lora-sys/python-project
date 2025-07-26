9817c903b77c7c07d0a22f3694d32b263bf0e507
lora <3526039967@qq.com>
2025-07-26 09:40:04 +0800

    first machine learn project iris dataset

---
+++ test.py
@@ -0,0 +1,12 @@
+import sys
+import scipy
+import numpy
+import pandas
+import sklearn
+
+# Print the versions of the libraries
+print('Python: {}'.format(sys.version))
+print('scipy: {}'.format(scipy.__version__))  # Corrected attribute name
+print('numpy: {}'.format(numpy.__version__))
+print('pandas: {}'.format(pandas.__version__))
+print('sklearn: {}'.format(sklearn.__version__))
